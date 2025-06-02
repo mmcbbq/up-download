3515fef29c23b396afba8c5c77e0c86bcad5553f
mmcbbq <m.martinez@bbq.de>
2025-06-02 14:44:19 +0200

    color

--- data.php
+++ data.php
@@ -7,35 +7,65 @@
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Document</title>
 </head>
-<body>
-<div>
-<?php
-$upload = $_GET['upload'] ?? false;
-$pw = $_GET['wrong'] ?? false;
-$dir = opendir('./data');
-while (false !== ($file = readdir($dir))){
-    if ($file != '.' and $file != '..'){
-    echo "<a href='data/$file' download='$file'>$file</a><br>";
+<body style='background-color: #FB8F67'>
+<div style='background-color: #FFC2B4'>
+    <h1>Files</h1>
+    <?php
+    $upload = $_GET['upload'] ?? false;
+    $pw = $_GET['wrong'] ?? false;
+    $dir = opendir('./data');
+    $count = 0;
+    echo '<div style="width: 50%; border: rosybrown; border-style: solid ">';
+
+    while (false !== ($file = readdir($dir))) {
+        if ($count % 2 == 0) {
+            $color = '#46D0D8';
+        } else {
+            $color = '#00c49a';
+        }
+        $count++;
+        if ($file != '.' and $file != '..') {
+            echo "<div style='background-color: $color'>";
+            echo "<a href='data/$file' download='$file' style='color: inherit; text-decoration: inherit' ><div>$file</div></a><br>";
+            echo "</div>";
+        }
     }
+
+    echo '</div> <br>';
+
+    ?>
+</div>
+<br>
+<br>
+<br>
+<div style='background-color: #F8E16C'>
+    <h1>Upload</h1>
+<div>
+
+    <form action='upload.php' method='post' enctype='multipart/form-data'>
+        <input type='file' name='file' id='file'><br>
+        Passwort    <input type='password' name='password' id='password'><br>
+        <input type='submit' value='upload' name='submit'>
+    </form>
+</div>
+<?php
+if ($pw) {
+    echo "<div style='color: #ff0019' >Falsches PW</div>";
 }
-if ($pw){
-    echo "<div style='color: red' >Falsches PW</div>";
-}
-if ($upload){
+if ($upload) {
     echo "<div style='color: red'> Upload OK</div>";
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    echo "post";
+}
 ?>
+
+
+
+<br>
+
 </div>
-
-<div>
-
-<form action='upload.php' method='post' enctype='multipart/form-data'>
-    <input type='file' name='file' id='file'><br>
-    <input type='password' name='password' id='password'><br>
-    <input type='submit' value='upload' name='submit'>
-</form>
-</div>
-
 </body>
 </html>
 
